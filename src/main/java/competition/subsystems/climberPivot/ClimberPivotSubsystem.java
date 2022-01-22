f4d6ddfb0d70811d724c6a4ad5ccfd083ab2ee96
hetaskesen <helintaskesen@gmail.com>
2022-01-22 11:36:01 -0800

    Saving climberpivot files

---
+++ src/main/java/competition/subsystems/climberPivot/ClimberPivotSubsystem.java
@@ -0,0 +1,24 @@
+package competition.subsystems.climberPivot;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import xbot.common.command.BaseCommand;
+import xbot.common.command.BaseSubsystem;
+import xbot.common.controls.actuators.XSolenoid;
+import xbot.common.injection.wpi_factories.CommonLibFactory;
+
+@Singleton
+public class ClimberPivotSubsystem extends BaseSubsystem{
+    public XSolenoid pivot;
+
+    @Inject
+    public ClimberPivotSubsystem(CommonLibFactory factory) {
+        pivot = factory.createSolenoid(2);
+    }
+    public void pivoteIn() {
+        pivot.setOn(false);
+    }
+    public void pivotOut() {
+        pivot.setOn(true);
+    }
