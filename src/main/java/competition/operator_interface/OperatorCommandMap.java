4a8780b8e19ea220c7c8db3db554f81e4029f28d
hetaskesen <helintaskesen@gmail.com>
2022-02-08 20:43:53 -0800

    Climber buttons, removed an unused import on climber arm subsystem

--- src/main/java/competition/operator_interface/OperatorCommandMap.java
+++ src/main/java/competition/operator_interface/OperatorCommandMap.java
@@ -7,6 +7,11 @@ import competition.injection.swerve.FrontLeftDrive;
 import competition.injection.swerve.FrontRightDrive;
 import competition.injection.swerve.RearLeftDrive;
 import competition.injection.swerve.RearRightDrive;
+import competition.subsystems.climber_arm.ClimberArmSubsystem;
+import competition.subsystems.climber_arm.commands.MotorArmExtendCommand;
+import competition.subsystems.climber_arm.commands.MotorArmRetractCommand;
+import competition.subsystems.climber_pivot.commands.PivotInCommand;
+import competition.subsystems.climber_pivot.commands.PivotOutCommand;
 import competition.subsystems.drive.commands.CalibrateSteeringCommand;
 import competition.subsystems.drive.commands.DebuggingSwerveWithJoysticksCommand;
 import competition.subsystems.drive.commands.GoToNextActiveSwerveModuleCommand;
@@ -14,7 +19,10 @@ import competition.subsystems.drive.commands.SimpleCrabDriveFromGamepadCommand;
 import competition.subsystems.drive.commands.SwerveDriveMaintainerCommand;
 import competition.subsystems.drive.commands.SwerveDriveWithJoysticksCommand;
 import competition.subsystems.drive.commands.SwerveSteeringMaintainerCommand;
+import competition.subsystems.latch.commands.LatchArmCommand;
+import competition.subsystems.latch.commands.LatchReleaseCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import xbot.common.logic.Latch;
 import xbot.common.subsystems.pose.commands.SetRobotHeadingCommand;
 
 /**
@@ -43,8 +51,25 @@ public class OperatorCommandMap {
         operatorInterface.driverGamepad.getifAvailable(4).whenPressed(crabDrive);
         operatorInterface.driverGamepad.getifAvailable(2).whenPressed(swerveDebugging);
         operatorInterface.driverGamepad.getifAvailable(3).whenPressed(goToNextActiveSwerveModule);
-    }
 
+    }
+    @Inject
+    public void setupclimbingCommands(
+        OperatorInterface operatorInterface,
+        MotorArmExtendCommand extendArmCommand,
+        MotorArmRetractCommand retractArmCommand,
+        LatchArmCommand latchArm,
+        LatchReleaseCommand releaseArm,
+        PivotInCommand pivotIn,
+        PivotOutCommand pivotOut
+    ){
+        operatorInterface.driverGamepad.getifAvailable(9).whenPressed(extendArmCommand);
+        operatorInterface.driverGamepad.getifAvailable(10).whenPressed(retractArmCommand);
+        operatorInterface.driverGamepad.getifAvailable(8).whenPressed(latchArm);
+        operatorInterface.driverGamepad.getifAvailable(7).whenPressed(releaseArm);
+        operatorInterface.driverGamepad.getifAvailable(5).whenPressed(pivotIn);
+        operatorInterface.driverGamepad.getifAvailable(6).whenPressed(pivotOut);
+    }
     @Inject
     public void setupGeneralSwerveCommands(
         OperatorInterface operatorInterface,
--- src/main/java/competition/subsystems/climber_arm/ClimberArmSubsystem.java
+++ src/main/java/competition/subsystems/climber_arm/ClimberArmSubsystem.java
@@ -5,7 +5,6 @@ import com.google.inject.Singleton;
 
 import xbot.common.command.BaseSetpointSubsystem;
 import xbot.common.controls.actuators.XCANSparkMax;
-import xbot.common.injection.electrical_contract.DeviceInfo;
 import xbot.common.injection.wpi_factories.CommonLibFactory;
 import xbot.common.math.MathUtils;
 
