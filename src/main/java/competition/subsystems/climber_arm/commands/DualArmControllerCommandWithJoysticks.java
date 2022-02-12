7ab755b7cdded5b8a2f5da37e15865f83480e761
John Gilbert <johngilb@gmail.com>
2022-02-11 18:52:44 -0800

    Make deadband easier to manage

--- src/main/java/competition/subsystems/climber_arm/commands/DualArmControllerCommandWithJoysticks.java
+++ src/main/java/competition/subsystems/climber_arm/commands/DualArmControllerCommandWithJoysticks.java
@@ -6,6 +6,7 @@ import competition.injection.arm.LeftArm;
 import competition.injection.arm.RightArm;
 import competition.operator_interface.OperatorInterface;
 import competition.subsystems.climber_arm.ClimberArmSubsystem;
+import xbot.common.math.MathUtils;
 
 public class DualArmControllerCommandWithJoysticks extends BaseDoubleClimberArmCommand {
 
@@ -32,6 +33,10 @@ public class DualArmControllerCommandWithJoysticks extends BaseDoubleClimberArmC
     public void execute() {
         double leftArmPower = oi.operatorGamepad.getLeftStickY();
         double rightArmPower = oi.operatorGamepad.getRightStickY();
+
+        // Use a simple deadband
+        leftArmPower = MathUtils.deadband(leftArmPower, oi.getOperatorGamepadTypicalDeadband(), (a) -> a);
+        rightArmPower = MathUtils.deadband(rightArmPower, oi.getOperatorGamepadTypicalDeadband(), (a) -> a);
 
         leftArm.setPower(leftArmPower, isSafe);
         rightArm.setPower(rightArmPower, isSafe);
