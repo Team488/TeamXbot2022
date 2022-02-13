d10d5b13524b186accb4b2e9936288e4ba861bc6
Stephen Just <stephenjust@gmail.com>
2022-02-12 23:13:04 -0800

    Remove extra import

--- src/test/java/competition/subsystems/drive/swerve/commands/SetSteeringMotorControllerPidParametersCommandTest.java
+++ src/test/java/competition/subsystems/drive/swerve/commands/SetSteeringMotorControllerPidParametersCommandTest.java
@@ -3,8 +3,6 @@ package competition.subsystems.drive.swerve.commands;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
-
 import org.junit.Test;
 
 import competition.BaseCompetitionTest;
