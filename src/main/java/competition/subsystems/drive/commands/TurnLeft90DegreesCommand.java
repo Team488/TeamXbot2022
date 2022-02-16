39aed8f6818b24a2fa3154f6ac5c1e43665d1d32
JohnGilb <johngilb@gmail.com>
2022-02-15 20:22:11 -0800

    use newer commonlib

--- src/main/java/competition/subsystems/drive/commands/TurnLeft90DegreesCommand.java
+++ src/main/java/competition/subsystems/drive/commands/TurnLeft90DegreesCommand.java
@@ -7,21 +7,27 @@ import competition.subsystems.pose.PoseSubsystem;
 import edu.wpi.first.math.geometry.Rotation2d;
 import xbot.common.command.BaseCommand;
 import xbot.common.injection.wpi_factories.CommonLibFactory;
+import xbot.common.math.XYPair;
+import xbot.common.subsystems.drive.control_logic.HeadingModule;
 
+/**
+ * Simple command to help us tune the drive's rotation PID.
+ */
 public class TurnLeft90DegreesCommand extends BaseCommand {
 
     private Rotation2d initialHeading;
     private Rotation2d goalHeading;
     PoseSubsystem pose;
     DriveSubsystem drive;
+    HeadingModule headingModule;
 
     @Inject
     public TurnLeft90DegreesCommand(DriveSubsystem drive, PoseSubsystem pose, CommonLibFactory clf) {
         this.addRequirements(drive);
-        clf.createHeadingModule(drive.getRotateToHeadingPid());
-
+        
         this.drive = drive;
         this.pose = pose;
+        this.headingModule = clf.createHeadingModule(drive.getRotateToHeadingPid());
     }
 
     @Override
@@ -33,6 +39,13 @@ public class TurnLeft90DegreesCommand extends BaseCommand {
 
     @Override
     public void execute() {
+        double power = headingModule.calculateHeadingPower(goalHeading);
+        // Just rotate.
+        drive.move(new XYPair(), -power);
+    }
 
+    @Override
+    public boolean isFinished() {
+        return headingModule.isOnTarget();
     }
 }
