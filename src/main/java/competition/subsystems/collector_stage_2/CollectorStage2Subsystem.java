eb636c75b1ef36dc739bc6b0c565de053d35223f
Alex Schokking <aschokking@gmail.com>
2022-02-17 20:09:23 -0800

    Guard is ready on power set too

--- src/main/java/competition/subsystems/collector_stage_2/CollectorStage2Subsystem.java
+++ src/main/java/competition/subsystems/collector_stage_2/CollectorStage2Subsystem.java
@@ -5,6 +5,7 @@ import com.google.inject.Singleton;
 
 import competition.electrical_contract.ElectricalContract;
 import xbot.common.controls.actuators.XCANSparkMax;
+import xbot.common.controls.actuators.mock_adapters.MockCANSparkMax;
 import xbot.common.injection.wpi_factories.CommonLibFactory;
 import xbot.common.properties.PropertyFactory;
 import xbot.common.subsystems.simplemotor.SimpleMotorSubsystem;
@@ -13,18 +14,23 @@ import xbot.common.subsystems.simplemotor.SimpleMotorSubsystem;
 public class CollectorStage2Subsystem extends SimpleMotorSubsystem {
 
     public final XCANSparkMax motor;
+    public final boolean isReady;
 
     @Inject
     public CollectorStage2Subsystem(PropertyFactory pf, CommonLibFactory clf, ElectricalContract eContract) {
         super("CollectorStage2", pf);
-        if(eContract.isCollectorStage2Ready()) {
+        this.isReady = eContract.isCollectorStage2Ready();
+        if(isReady) {
             motor = clf.createCANSparkMax(eContract.getCollectorStage2Motor(), this.getPrefix(), "Collector Stage2 Motor");
+        } else {
+            motor = null;
         }
     }
 
     @Override
     public void setPower(double power) {
-        motor.set(power);
+        if(isReady) {
+            motor.set(power);
+        }
     }
-    
 }
