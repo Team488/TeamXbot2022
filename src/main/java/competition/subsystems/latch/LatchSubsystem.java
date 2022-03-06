69fa5efc8299358a065b1b72abd133220308b1e0
Stephen Just <stephenjust@gmail.com>
2022-03-05 19:49:45 -0800

    Wrap latch in contract checks to reduce exceptions, Remove duplicate ids in Sim contract

--- src/main/java/competition/subsystems/latch/LatchSubsystem.java
+++ src/main/java/competition/subsystems/latch/LatchSubsystem.java
@@ -13,11 +13,13 @@ import xbot.common.injection.wpi_factories.CommonLibFactory;
 public class LatchSubsystem extends BaseSubsystem {
     public XDoubleSolenoid latch;
 
+    private final ElectricalContract contract;
     private int numberOfUnlatches = 0;
 
     @Inject
     public LatchSubsystem(CommonLibFactory factory, ElectricalContract contract) {
-        
+        this.contract = contract;
+
         if (contract.arePneumaticsReady()) {
             latch = factory.createDoubleSolenoid(
                 factory.createSolenoid(contract.getLatchSolenoid().channel), 
@@ -27,10 +29,15 @@ public class LatchSubsystem extends BaseSubsystem {
     }
 
     public void arm(){
-        latch.setDoubleSolenoid(DoubleSolenoidMode.FORWARD);
+        if (contract.arePneumaticsReady()) {
+            latch.setDoubleSolenoid(DoubleSolenoidMode.FORWARD);
+        }
     }
     public void release(){
-        latch.setDoubleSolenoid(DoubleSolenoidMode.REVERSE);
+        if (contract.arePneumaticsReady()) {
+            latch.setDoubleSolenoid(DoubleSolenoidMode.REVERSE);
+        }
+        
         numberOfUnlatches++;
     }
 
