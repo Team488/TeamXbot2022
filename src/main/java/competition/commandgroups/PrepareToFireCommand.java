a23a737a9dc405d98e30b09723ce1a6925862723
Stephen Just <stephenjust@gmail.com>
2022-03-08 19:59:24 -0800

    Split conditional logic to a seaprate command

--- src/main/java/competition/commandgroups/PrepareToFireCommand.java
+++ src/main/java/competition/commandgroups/PrepareToFireCommand.java
@@ -6,7 +6,7 @@ import com.google.inject.Inject;
 
 import org.apache.log4j.Logger;
 
-import competition.subsystems.conveyer.ConveyorSubsystem;
+import competition.subsystems.conveyer.commands.ReverseConveyorIfCollectorStoppedCommand;
 import competition.subsystems.shooterwheel.ShooterWheelSubsystem;
 import competition.subsystems.shooterwheel.ShooterWheelSubsystem.TargetRPM;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
@@ -22,27 +22,35 @@ public class PrepareToFireCommand extends SequentialCommandGroup {
     private final DoubleProperty conveyorReverseTimeProp;
     private final DoubleProperty waitTimeProp;
     private final ShooterWheelSubsystem wheel;
-    private final ConveyorSubsystem conveyor;
     private Supplier<Double> externalWaitSupplier;
     private Supplier<Double> externalTargetSupplier;
 
     private static Logger log = Logger.getLogger(PrepareToFireCommand.class);
 
     @Inject
-    PrepareToFireCommand(ShooterWheelSubsystem wheel, ConveyorSubsystem conveyor, PropertyFactory pf) {
+    PrepareToFireCommand(ShooterWheelSubsystem wheel,
+            ReverseConveyorIfCollectorStoppedCommand reverseConveyorCommand, PropertyFactory pf) {
         pf.setPrefix(this.getName());
         conveyorReverseTimeProp = pf.createPersistentProperty("Conveyor Reverse Time", 0.2);
         waitTimeProp = pf.createPersistentProperty("Max Wait Time", 5);
         this.wheel = wheel;
-        this.conveyor = conveyor;
 
-        // We don't know which commands to run in sequence until initialize
+        // We will need to reverse the conveyor for a litle bit before spinning up the shooter to make sure
+        // nothing is touching the wheel
+        var reverseConveyor = new ParallelRaceGroup(
+            reverseConveyorCommand,
+            new DelayViaSupplierCommand(() -> conveyorReverseTimeProp.get())
+        );
+
+        var setTargetRPM = new NamedInstantCommand("ApplyShooterTargetRPM", () -> applyTargetRPM(), wheel.getSetpointLock());
+        var waitForReadiness = new SimpleWaitForMaintainerCommand(wheel, getWaitTime());
+
+        addCommands(reverseConveyor, setTargetRPM, waitForReadiness);
     }
 
     @Override
     public void initialize() {
         log.info("Initializing");
-        prepareCommands();
         super.initialize();
     }
 
@@ -67,22 +75,5 @@ public class PrepareToFireCommand extends SequentialCommandGroup {
             return () -> waitTimeProp.get();
         }
         return externalWaitSupplier;
-    }
-
-    private void prepareCommands() {
-        // We will need to reverse the conveyor for a litle bit before spinning up the shooter to make sure
-        // nothing is touching the wheel
-        if (wheel.getTargetRPM() == 0) {
-            var reverseConveyor = new ParallelRaceGroup(
-                conveyor.getReverseCommand(),
-                new DelayViaSupplierCommand(() -> conveyorReverseTimeProp.get())
-            );
-            addCommands(reverseConveyor);
-        }
-
-        var setTargetRPM = new NamedInstantCommand("ApplyShooterTargetRPM", () -> applyTargetRPM(), wheel.getSetpointLock());
-        var waitForReadiness = new SimpleWaitForMaintainerCommand(wheel, getWaitTime());
-
-        addCommands(setTargetRPM, waitForReadiness);
     }
 }
