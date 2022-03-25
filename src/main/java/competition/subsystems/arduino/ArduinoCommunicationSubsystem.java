510e5e45f0137ae0c4fe32ec01f145b6b4866c77
JohnGilb <johngilb@gmail.com>
2022-03-24 20:46:57 -0700

    Arduino mystery

--- src/main/java/competition/subsystems/arduino/ArduinoCommunicationSubsystem.java
+++ src/main/java/competition/subsystems/arduino/ArduinoCommunicationSubsystem.java
@@ -5,6 +5,7 @@ import com.google.inject.Singleton;
 
 import competition.electrical_contract.ElectricalContract;
 import competition.subsystems.pose.PoseSubsystem;
+import competition.subsystems.shooterwheel.ShooterWheelSubsystem;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import xbot.common.command.BaseSubsystem;
@@ -18,7 +19,7 @@ import xbot.common.properties.StringProperty;
 
 @Singleton
 public class ArduinoCommunicationSubsystem extends BaseSubsystem {
-    
+
     XDigitalOutput dio0;
     XDigitalOutput dio1;
     XDigitalOutput dio2;
@@ -32,6 +33,7 @@ public class ArduinoCommunicationSubsystem extends BaseSubsystem {
     private final int loopMod = 5;
 
     private final PoseSubsystem pose;
+    private final ShooterWheelSubsystem wheel;
 
     private final StringProperty chosenState;
     private final BooleanProperty dio0Property;
@@ -45,7 +47,7 @@ public class ArduinoCommunicationSubsystem extends BaseSubsystem {
         RobotNotBooted(0),
         RobotDisabled(1),
         RobotEnabled(2),
-        RobotNotLevel(4);
+        Celebration(4);
 
         private int value;
 
@@ -57,10 +59,12 @@ public class ArduinoCommunicationSubsystem extends BaseSubsystem {
             return value;
         }
     }
-    
+
     @Inject
-    public ArduinoCommunicationSubsystem(CommonLibFactory clf, ElectricalContract contract, PropertyFactory pf, PoseSubsystem pose) {
+    public ArduinoCommunicationSubsystem(CommonLibFactory clf, ElectricalContract contract, PropertyFactory pf,
+            PoseSubsystem pose, ShooterWheelSubsystem wheel) {
         this.pose = pose;
+        this.wheel = wheel;
 
         dio0 = clf.createDigitalOutput(contract.getArduinoDio0().channel);
         dio1 = clf.createDigitalOutput(contract.getArduinoDio1().channel);
@@ -83,15 +87,15 @@ public class ArduinoCommunicationSubsystem extends BaseSubsystem {
 
         this.register();
     }
- 
+
     int counter = 0;
 
     @Override
     public void periodic() {
         // We don't need to run this every cycle.
-        if (this.loopCounter++ % loopMod != 0) {
+        /*if (this.loopCounter++ % loopMod != 0) {
             return;
-        }
+        }*/
 
         boolean dsEnabled = DriverStation.isEnabled();
         boolean isRedAlliance = DriverStation.getAlliance() == Alliance.Red;
@@ -105,8 +109,8 @@ public class ArduinoCommunicationSubsystem extends BaseSubsystem {
         if (!dsEnabled) {
             currentState = ArduinoStateMessage.RobotDisabled;
         } else if (dsEnabled) {
-            if (!pose.isRobotLevel()) {
-                currentState = ArduinoStateMessage.RobotNotLevel;
+            if (wheel.isMaintainerAtGoal()) {
+                currentState = ArduinoStateMessage.Celebration;
             } else {
                 currentState = ArduinoStateMessage.RobotEnabled;
             }
@@ -117,21 +121,41 @@ public class ArduinoCommunicationSubsystem extends BaseSubsystem {
         // index 3 (picking up that leftmost 1). The dioOutputs array is ordered
         // such that the element at index 3 is dio0 to match how the arduino currently
         // parses the binary data.
-        int stateValue = currentState.getValue();
-        for (int i = 3; i >=0; i--) {
+        //int stateValue = currentState.getValue();
+        
+        /*for (int i = 3; i >= 0; i--) {
             dioOutputs[3 - i].set(!((stateValue & (1 << i)) != 0));
+        }*/
+
+        switch (currentState) {
+            case RobotDisabled:
+                dio0.set(false);
+                dio1.set(true);
+                dio2.set(true);
+                dio3.set(true);
+                break;
+            case RobotEnabled:
+                dio0.set(true);
+                dio1.set(false);
+                dio2.set(true);
+                dio3.set(true);
+                break;
+            case Celebration: 
+                dio0.set(true);
+                dio1.set(true);
+                dio2.set(false);
+                dio3.set(false);
+                break;
+            default:
+                break;
         }
-
-        // Sweep through PWM values for now.
-        analogOutput.setRaw(counter % 255);
-        counter++;
-
+        /*
         chosenState.set(currentState.toString());
         dio0Property.set(dio0.get());
         dio1Property.set(dio1.get());
         dio2Property.set(dio2.get());
         dio3Property.set(dio3.get());
         allianceDioProperty.set(allianceDio.get());
-        analogOutputProperty.set(analogOutput.getRaw());
+        analogOutputProperty.set(analogOutput.getRaw());*/
     }
 }
