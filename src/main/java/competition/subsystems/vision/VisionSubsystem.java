58ee478338f412abe8fe9fa4d0dc394fb2479f7d
Stephen Just <stephenjust@gmail.com>
2022-04-01 22:59:29 -0700

    Add a TimeStableValidator on the fix

--- src/main/java/competition/subsystems/vision/VisionSubsystem.java
+++ src/main/java/competition/subsystems/vision/VisionSubsystem.java
@@ -6,25 +6,35 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import xbot.common.command.BaseSubsystem;
 import xbot.common.logging.RobotAssertionManager;
+import xbot.common.logic.TimeStableValidator;
 import xbot.common.properties.BooleanProperty;
 import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.PropertyFactory;
 
 public class VisionSubsystem extends BaseSubsystem {
 
+    public static final String VISION_TABLE = "photonvision";
+    public static final String FIX_ACQUIRED_PROPERTY = "gloworm/hasTarget";
+    public static final String TARGET_YAW_PROPERTY = "gloworm/targetYaw";
+
     final RobotAssertionManager assertionManager;
     final BooleanProperty isInverted;
     final DoubleProperty yawOffset;
+    final DoubleProperty waitForStableFixTime;
+    final TimeStableValidator fixIsStable;
     NetworkTable visionTable;
 
     @Inject
     public VisionSubsystem(PropertyFactory pf, RobotAssertionManager assertionManager) {
         this.assertionManager = assertionManager;
-        visionTable = NetworkTableInstance.getDefault().getTable("photonvision");
+        visionTable = NetworkTableInstance.getDefault().getTable(VISION_TABLE);
 
         pf.setPrefix(this);
         isInverted = pf.createPersistentProperty("Yaw inverted", true);
         yawOffset = pf.createPersistentProperty("Yaw offset", 0);
+
+        waitForStableFixTime = pf.createPersistentProperty("Fix stable time", 0.1);
+        fixIsStable = new TimeStableValidator(() -> waitForStableFixTime.get());
     }
 
     public double getBearingToHub() {
@@ -36,13 +46,14 @@ public class VisionSubsystem extends BaseSubsystem {
     }
 
     public boolean getFixAcquired() {
-        boolean fixAcquired = visionTable.getEntry("gloworm/hasTarget").getBoolean(false);
+        boolean fixAcquired = visionTable.getEntry(FIX_ACQUIRED_PROPERTY).getBoolean(false);
+        boolean isStable = fixIsStable.checkStable(fixAcquired);
 
-        return fixAcquired;
+        return fixAcquired && isStable;
     }
 
     private double getYawToTarget() {
-        double yawToTarget = (visionTable.getEntry("gloworm/targetYaw").getDouble(0) + this.yawOffset.get()) * getInversionFactor();
+        double yawToTarget = (visionTable.getEntry(TARGET_YAW_PROPERTY).getDouble(0) + this.yawOffset.get()) * getInversionFactor();
 
         return yawToTarget;
     }
